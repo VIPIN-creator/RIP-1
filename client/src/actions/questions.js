44432cca42493f05d305a62f046ae59dd10b8893
ashirbadIndia <ashirbad.behera.india@gmail.com>
2023-06-29 21:09:09 +0530

    Question List

--- client/src/actions/questions.js
+++ client/src/actions/questions.js
@@ -19,3 +19,31 @@ export const getQuestionDetails = (question_id) => async (dispatch)=>{
         });
     }
 }
+
+export const getAllQuestions = () => async (dispatch) => {
+    try {
+        const response = await question.get('/');
+        console.log(response.data);
+        dispatch({
+            type: 'GET_ALL_QUESTION',
+            response: response.data,
+            error: false
+        });
+    }
+    catch(e){
+        console.log(e);
+        dispatch({
+            type: 'GET_ALL_QUESTION',
+            error: true,
+            response: e.response
+        });
+    }
+}
+
+export const setQuestionId = (id) => async (dispatch) => {
+    dispatch({
+        type: 'SET_QUESTION_ID',
+        response: id,
+        error: false
+    });
+}
